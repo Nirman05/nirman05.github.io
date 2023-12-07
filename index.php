0e94c0d582ffc425f6ad4165ed671832d661e1ea
Nirman05 <nirmanjr97@gmail.com>
2023-12-07 20:49:51 +0800

    foto

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+  <!-- bostrap icon -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+
+  <!-- my css -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="style.css" />
+  <title>Portofolio</title>
+</head>
+
+<body id="body">
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Moh Nirman</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#body">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">My Motto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#project">Project</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#kontak">kontak</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- akhir nav -->
+  <!-- jumbrotn -->
+  <section class="jumbotron text-center an">
+    <img src="img/Nirman.png" alt="Aan" class="rounded-circle img-thumbnail" />
+    <h1 class="display-4 fw-bold">Mohammad Nirman</h1>
+    <p class="lead">Mahasiswa | Conten Creator</p>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,64L48,85.3C96,107,192,149,288,154.7C384,160,480,128,576,138.7C672,149,768,203,864,197.3C960,192,1056,128,1152,106.7C1248,85,1344,107,1392,117.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- akhir jumb -->
+  <section id="about">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>My Motto</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-4">
+          <p>“ Kesuksesan itu berawal dari sebuah impian, dan impian hadir disaat kita tidur. Jadi jika kamu semua ingin sukses maka lebih baik tidurlah sekarang juga”</p>
+          <p>“ Kegagalan adalah keberhasilan yang tertunda maka kebohongan adalah kejujuran yang tertunda.”</p>
+        </div>
+        <div class="col-md-4">
+          <p>“ Jika anda dilempar batu oleh seseorang, janganlah melempar balik kepadanya. Balaslah dengan senyuman dan lempar bunga kepadanya.. Tapi usahakan potnya ikut terlempar.”</p>
+          <p>" Diam tak akan menyelesaikan masalah, tapi diam juga tak akan menimbulkan masalah."</p>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#e2edff" fill-opacity="1" d="M0,192L48,197.3C96,203,192,213,288,218.7C384,224,480,224,576,192C672,160,768,96,864,101.3C960,107,1056,181,1152,218.7C1248,256,1344,256,1392,256L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- akhir -->
+  <!-- awal pro -->
+  <section id="project">
+    <div class="container">
+      <div class="row text-center text-center mb-3">
+        <div class="col">
+          <h2>My Project</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/1.jpg" class="card-img-top" alt="ann" />
+            <div class="card-body">
+              <p class="card-text">“ Tidak ada pekerjaan yang berat, seberat-beratnya pekerjaan maka akan terasa ringan jika kita tidak mengerjakannya.”</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/2.jpg" class="card-img-top" alt="ann" />
+            <div class="card-body">
+              <p class="card-text">“ Jika hari ini kamu gagal, maka jangan pernah menyerah. Ulangi terus kegagalan kamu sampai boss kamu menyerah.”</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/3.jpg" class="card-img-top" alt="ann" />
+            <div class="card-body">
+              <p class="card-text">" Kerja keraslah sampai tetangga berpikir rezekimu hasil dari pesugihan."</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/4.jpg" class="card-img-top" alt="ann" />
+            <div class="card-body">
+              <p class="card-text">" Jangan mengulangi kesalahan yang sama. Masih banyak kesalahan-kesalahan lain yang perlu dicoba.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/5.jpg" class="card-img-top" alt="ann" />
+            <div class="card-body">
+              <p class="card-text">" Kamu tidak butuh kata-kata motivasi, yang kamu butuhkan adalah kata 'diskon besar-besaran'."</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,224L48,234.7C96,245,192,267,288,245.3C384,224,480,160,576,165.3C672,171,768,245,864,256C960,267,1056,213,1152,186.7C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- akhir -->
+  <!-- awal contact -->
+  <section id="kontak">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col-md">
+          <h2>Contact</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="alert alert-warning alert-dismissible fade show d-none my-alert" role="alert">
+            <strong>Terimaksi !</strong> Pesan Anda Sudah Kami Terima
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          <form name="kontak-form">
+            <div class="mb-3">
+              <label for="name" class="form-label">Nama Lengkap</label>
+              <input type="nama" class="form-control" id="name" aria-describedby="name" name="nama" />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" aria-describedby="email" name="email" />
+            </div>
+            <div class="mb-3">
+              <label for="pesan" class="form-label">Pesan</label>
+              <textarea class="form-control" id="pesan" rows="3" name="pesan" placeholder="Pesan Nya Boleh Apa Saja Curhat Pun Boleh :)"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-kirim">Kirim</button>
+            <button class="btn btn-primary btn-loading d-none" type="button" disabled>
+              <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+              Loading...
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#0d6efd" fill-opacity="1" d="M0,192L26.7,186.7C53.3,181,107,171,160,186.7C213.3,203,267,245,320,234.7C373.3,224,427,160,480,160C533.3,160,587,224,640,245.3C693.3,267,747,245,800,245.3C853.3,245,907,267,960,261.3C1013.3,256,1067,224,1120,176C1173.3,128,1227,64,1280,58.7C1333.3,53,1387,107,1413,133.3L1440,160L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- akhir kontak -->
+  <!-- footer -->
+  <footer class="bg-primary text-white text-center pb-3">
+    <p>Create With <i class="bi bi-heart-fill text-danger"></i> by <a href="https://www.instagram.com/nirman_jr24/" class="text-white fw-bold">Nirman Jr</a></p>
+  </footer>
+  <!-- foter -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  <script>
+    const scriptURL = "https://script.google.com/macros/s/AKfycbyHr3Tul-0yq8MyZ_mqWdx1NhFny-XCnl0eWSIjZb-g0E5yblHXnu6IZ7yBbJ7U0lHb/exec";
+    const form = document.forms["kontak-form"];
+    const btnKirim = document.querySelector(".btn-kirim");
+    const btnLoading = document.querySelector(".btn-loading");
+    const myAlert = document.querySelector(".my-alert");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      // ketika tombol di klik
+      // tampilkan tombol loading hilangkan tombol kirim
+
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+
+      fetch(scriptURL, {
+          method: "POST",
+          body: new FormData(form)
+        })
+        .then((response) => {
+          btnLoading.classList.toggle("d-none");
+          btnKirim.classList.toggle("d-none");
+          // tampilkan alert
+          myAlert.classList.toggle("d-none");
+          // reset from
+          form.reset();
+          console.log("Success!", response);
+        })
+        .catch((error) => console.error("Error!", error.message));
+    });
+  </script>
+  <script>
+    document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+  </script>
+
+  <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
